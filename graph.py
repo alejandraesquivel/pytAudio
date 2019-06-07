3c10839f230df054182b76242ae69c898307eef9
karen <karenreyes346@gmail.com>
2019-06-07 09:36:06 -0500

    GUI sin clases para 1 senal

--- graph.py
+++ graph.py
@@ -34,7 +34,7 @@ def draw_figure(canvas, figure, loc=(0, 0)):
 # Create a canvas
 w, h = 900,600
 window = tk.Tk()
-window.title("A figure in a canvas")
+window.title("Proyecto final, Analisis con 1 senal")
 canvas = tk.Canvas(window, width=w, height=h)
 canvas.pack()
 
@@ -43,44 +43,32 @@ X = np.linspace(0, 2 * np.pi, 50)       #graficas a poner
 Y = np.sin(X)
 
 # Create the figure we desire to add to an existing canvas
-fig = mpl.figure(figsize=(2, 1))
+fig = mpl.figure(figsize=(7, 2))
 ax = fig.add_axes([0, 0, 1, 1])
 ax.plot(X, Y)
 
 # Keep this handle alive, or else figure will disappear
-fig_x, fig_y = 100, 100
+fig_x, fig_y = 100, 75
 fig_photo = draw_figure(canvas, fig, loc=(fig_x, fig_y))
 fig_w, fig_h = fig_photo.width(), fig_photo.height()
 
 # Add more elements to the canvas, potentially on top of the figure
-canvas.create_text(200, 50, text="primera", anchor="s")
+canvas.create_text(150, 50, text="Grafica en el Dominio de la Frecuencia", anchor="s")
 
 
 # Create the figure we desire to add to an existing canvas
-fig2 = mpl.figure(figsize=(2, 1))
+fig2 = mpl.figure(figsize=(7, 2))
 ax2 = fig2.add_axes([0, 0, 1, 1])
 ax2.plot(X, Y)
 
 # Keep this handle alive, or else figure will disappear
-fig2_x, fig2_y = 100, 280
+fig2_x, fig2_y = 100,350
 fig2_photo = draw_figure(canvas, fig2, loc=(fig2_x, fig2_y))
 fig2_w, fig2_h = fig2_photo.width(), fig2_photo.height()
 
 # Add more elements to the canvas, potentially on top of the figure
-canvas.create_text(200,250, text="segunda", anchor="s")
+canvas.create_text(100,320, text="Resultado de Operaciones", anchor="s")
 
-# Create the figure we desire to add to an existing canvas
-fig3 = mpl.figure(figsize=(2, 1))
-ax3 = fig3.add_axes([0, 0, 1, 1])
-ax3.plot(X, Y)
-
-# Keep this handle alive, or else figure will disappear
-fig3_x, fig3_y = 100, 460
-fig3_photo = draw_figure(canvas, fig3, loc=(fig3_x, fig3_y))
-fig3_w, fig3_h = fig3_photo.width(), fig3_photo.height()
-
-# Add more elements to the canvas, potentially on top of the figure
-canvas.create_text(200,450, text="tercera", anchor="s")
 #menu de audios
 var=tk.StringVar(window)
 opciones=['audio1','audio2']
@@ -88,8 +76,16 @@ op=tk.OptionMenu(window,var,*opciones)
 op.config(width=30)
 op.pack(side='right',padx=1,pady=1)
 	
-btn =tk.Button(window, text="Click Me")
+btn =tk.Button(window, text="Reproducir")
 btn.pack(side='right',padx=5,pady=5)
 
+
+#boton menu
+v=tk.StringVar(window)
+opcioness=['Resolucion','Amplificacion','Modulacion']
+opo=tk.OptionMenu(window,v,*opcioness)
+opo.config(width=30)
+opo.pack(side='right',padx=1,pady=1)
+    
 # Let Tk take over
 tk.mainloop()
